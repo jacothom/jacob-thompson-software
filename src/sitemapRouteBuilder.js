04789e5168cb83f60c2fbe7f5d57d791b758ff83
Jacob <35601699+jacothom@users.noreply.github.com>
2021-05-02 21:05:01 -0400

    Remove boilerplate readme

--- src/sitemapRouteBuilder.js
+++
@@ -1,7 +0,0 @@
-async function getSitemapRoutes() {
-  const { $content } = require('@nuxt/content')
-  const posts = await $content('articles').fetch()
-  return posts.map((post) => `/blog/${post.slug}`)
-}
-
-export default getSitemapRoutes
